e257b1d0c99b9bb775dd99645daab2caf4c8bd93
Victor Pereira <149492884+victorpereira96@users.noreply.github.com>
2023-11-29 09:14:41 -0300

    Create ex 003 e 004

--- exercicios/ex003/script.js
+++ exercicios/ex003/script.js
@@ -1,14 +1,36 @@
 function contar() {
-    var inicio = document.querySelector('input#inicio')
-    var fim = document.querySelector('input#fim')
-    var passo = document.querySelector('input#passo')
-    var res = document.querySelector('div#res')
-    var inicio = Number(inicio.value)
-    var fim = Number(fim.value)
-    var passo = Number(passo.value)
-    c = 1
+    var ini = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var res = document.getElementById('res')
 
-    while (inicio < fim) {
-        res.innerHTML = 
-    }
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Falta dados!')
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando... <br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0) {
+            alert('PAsso inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem Crescente
+            for (var c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449} `
+            }
+            
+        } else {
+            // Contagem Regressiva
+            var c = i
+            while (c >= f) {
+                res.innerHTML += `${c} \u{1F449} `
+                c -= p
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+     } 
+    
 }
