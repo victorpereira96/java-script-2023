88a7fb76c0d8538fbd6d8aec3d5cb3b7abbf212c
Victor Pereira <149492884+victorpereira96@users.noreply.github.com>
2023-11-29 08:25:00 -0300

    atualizei

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,29 @@
+for (var c=1; c<=10; c++) {
+    console.log(`Passo ${c}`)
+}
+
+/*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c += 1
+} while(c <= 6)
+*/
+
+/*
+var c = 1
+while(c <= 1000) {
+    console.log(`Passo ${c}`)
+    c += 1
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ exercicios/ex003/script.js
@@ -0,0 +1,14 @@
+function contar() {
+    var inicio = document.querySelector('input#inicio')
+    var fim = document.querySelector('input#fim')
+    var passo = document.querySelector('input#passo')
+    var res = document.querySelector('div#res')
+    var inicio = Number(inicio.value)
+    var fim = Number(fim.value)
+    var passo = Number(passo.value)
+    c = 1
+
+    while (inicio < fim) {
+        res.innerHTML = 
+    }
+}
